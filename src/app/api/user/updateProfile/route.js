7647d3177c3b6943b86ebd21bee75c89d4149407
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-01-02 13:43:40 +0530

    update profile api

--- src/app/api/user/updateProfile/route.js
+++ src/app/api/user/updateProfile/route.js
@@ -1,13 +1,54 @@
 import connectDB from "../../../../lib/db";
 import User from "../../../models/user";
-export async function POST(req){
+import { NextResponse } from 'next/server';
+
+export async function POST(req) {
     try {
-        
+        const { userId, ProfileData } = await req.json();
+
+        // Destructure ProfileData
+        const { username, email, bio, profilePicture, socialMediaHandles } = ProfileData;
+
+        // Check if userId and ProfileData are provided
+        if (!userId || !ProfileData || typeof ProfileData !== 'object') {
+            return new NextResponse(
+                JSON.stringify({ error: 'User ID and Profile Data are required' }),
+                { status: 400 }
+            );
+        }
+
+        // Connect to the database
+        await connectDB();
+
+        // Find the user by userId
+        const user = await User.findById(userId);
+        if (!user) {
+            return new NextResponse(
+                JSON.stringify({ error: 'User not found' }),
+                { status: 404 }
+            );
+        }
+
+        // Update the user's profile fields
+        user.username = username || user.username;
+        user.email = email || user.email; // Assuming you also want to update the email
+        user.bio = bio || user.bio;
+        user.profilePicture = profilePicture || user.profilePicture;
+        user.socialMediaHandles =user.socialMediaHandles||socialMediaHandles;
+
+        // Save the updated user
+        await user.save();
+
+        // Return success response
+        return new NextResponse(
+            JSON.stringify({ message: 'Profile updated successfully' }),
+            { status: 200 }
+        );
     } catch (error) {
         console.error('Error updating Profile:', error);
-            return new NextResponse(
-              JSON.stringify({ error: 'Internal Server Error' }),
-              { status: 500 }
-            );
+        return new NextResponse(
+            JSON.stringify({ error: 'Internal Server Error' }),
+            { status: 500 }
+        );
     }
 }
