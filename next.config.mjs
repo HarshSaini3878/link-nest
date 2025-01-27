8fa5cfca96dd07686ffd656c0cc159e353f758be
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-01-27 14:04:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      optimizePackageImports: ["@chakra-ui/react"],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-        },
-      ],
-    },env: {
-      DATABASE_URL: process.env.DATABASE_URL,
-      AUTH_SECRET: process.env.AUTH_SECRET,
-      
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  },
+};
 
-CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
-CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
-CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET,
-
-NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    },
-  };
-  
-  export default nextConfig;
-  
+export default nextConfig;
