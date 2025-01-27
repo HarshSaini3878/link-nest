1074a26f7c74312405a77b703853993ccba46871
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-01-27 13:45:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
           hostname: 'res.cloudinary.com',
         },
       ],
+    },env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      AUTH_SECRET: process.env.AUTH_SECRET,
+      
+
+CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
+CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
+CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET,
+
+NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     },
   };
   
